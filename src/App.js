0c5119df5929aa771114eb7f054fe40d0f012a73
Your Name <your.email@example.com>
2024-10-10 23:06:55 +0530

    frontend-hashrouter-change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Main from './pages/Main';
 import Login from './Compoents/Login'; // Assuming you have a Login component
