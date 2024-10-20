20aaf35f294b310a2d1eaa468df24676f2d7a7bc
murugesh <dhanamurugesh1804@gmail.com>
2024-10-20 18:05:24 +0530

    new

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3500/', 
+  baseURL: 'http://68.233.99.191:5000', // Update to your Oracle VM IP and port
 });
