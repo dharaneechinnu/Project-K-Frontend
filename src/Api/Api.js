409bff90ddb2e83864e853eb85666b688d06cb6d
Your Name <your.email@example.com>
2024-10-23 21:09:17 +0530

    frontend-victory-connection

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -2,8 +2,4 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'http://140.245.214.34:3500/'
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 647b22be530b3c348040175567b905d2c426e7fb
