92d203ffd8a378aadbba2ccf4e57914f77812998
Your Name <your.email@example.com>
2024-10-23 21:06:10 +0530

    fronten-connect

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://68.233.99.191:5000', // Update to your Oracle VM IP and port
+  baseURL: 'http://140.245.214.34:3500/'
 });
