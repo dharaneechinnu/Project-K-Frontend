835aa4b3aafcf61e2e67b079605e12fdb174eac5
Dharaneedharan <117936700+dharaneechinnu@users.noreply.github.com>
2024-10-24 21:52:53 +0530

    Muruguesh updata oracal vm step-1

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,11 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' 
-    ? 'http://140.245.214.34/api'
-    : '/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  timeout: 10000,
+export default axios.create({
+  baseURL: 'http://140.245.214.34:4500'
 });
